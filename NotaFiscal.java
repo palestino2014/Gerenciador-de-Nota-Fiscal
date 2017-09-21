702d16379bb060e2915f1cdee5d19421a9949d4d
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-09-21 17:25:45 -0300

    Update NotaFiscal.java

--- NotaFiscal.java
+++ NotaFiscal.java
@@ -16,6 +16,17 @@ public class NotaFiscal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+         Produtos meuProduto;
+            meuProduto = new Produtos();
+            
+             meuProduto.nome = "desinfetante";
+             meuProduto.categoria = "Material de Limpeza";
+             meuProduto.codigo= 789456123 ;
+             meuProduto.preco = 12;
+
+             System.out.println("Produto cadastrado: " + meuProduto.nome  +   meuProduto.categoria  +  meuProduto.codigo  +   meuProduto.preco);
+
      
     }
     
