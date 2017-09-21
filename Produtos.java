f050827fa5f3b945fd481b1edbd87ec73cee0b3f
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-09-21 17:21:51 -0300

    Update Produtos.java

--- Produtos.java
+++ Produtos.java
@@ -11,4 +11,29 @@ package notafiscal;
  */
 public class Produtos {
     
+    int codigo;
+    String nome;
+    float preco;
+    String categoria;
+
+      void ins(String nome) {
+            String novoProduto = this.nome;
+             this.nome = novoProduto;
+            }
+
+      void cod(int codigo) {
+            int novoCodigo = this.codigo;
+            this.codigo = novoCodigo;
+            }
+
+      void valor(float preco){
+           float novoPreco = this.preco;
+           this.preco = this.preco;
+           }
+
+      void cat(String categoria){
+            String novaCategoria = this.categoria;
+            this.categoria = novaCategoria;
+        }
+    
 }
