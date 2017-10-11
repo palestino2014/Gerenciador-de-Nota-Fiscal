83c7fa987622ba8319b834cc8d7112dbe65a11b5
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-10 21:34:27 -0300

    Create DadosNotaFiscalEletronica.java

---
+++ DadosNotaFiscalEletronica.java
@@ -0,0 +1,24 @@
+package notafiscal;
+
+public class DadosNotaFiscalEletronica {
+
+    int serie;
+    int nNF;
+    String dhEmi;
+    String dhSaiEnt;
+    String tpNF;
+    String indPag;
+    String indPres;
+    String natOp;
+    String indFinal;
+    String idDest;
+
+    public DadosNotaFiscalEletronica() {
+        // <editor-fold defaultstate="collapsed" desc="Compiled Code">
+        /* 0: aload_0
+         * 1: invokespecial java/lang/Object."<init>":()V
+         * 4: return
+         *  */
+        // </editor-fold>
+    }
+}
