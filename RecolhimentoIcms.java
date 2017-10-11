33691d1f7ac250761d0dff292ced4a82a617d4e9
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-10 21:30:59 -0300

    Add files via upload

---
+++ RecolhimentoIcms.java
@@ -0,0 +1,48 @@
+package notafiscal;
+
+public class RecolhimentoIcms {
+
+    String icms;
+    int cst;
+    String icmssn101;
+    float pCredSN;
+    float vCredICMSSN;
+    String ICMSSN102;
+    String ICMSSN103;
+    float vBCUFDest;
+    float pICMSUFDest;
+    float pICMSInter;
+    float pICMSInterPart;
+    float vICMSUFDest;
+    float vICMSUFRemet;
+    float pFCPUFDest;
+    float vFCPUFDest;
+    String ICMSSN201t;
+    String ICMSSN202;
+    String ICMSSN203;
+    String modBC;
+    float pRedBC;
+    float vBC;
+    float pICMS;
+    float vICMSOp;
+    int CEST;
+    int modBCST;
+    float pMVAST;
+    float pRedBCST;
+    float vBCST;
+    float pICMSST;
+    float vICMSST;
+    int ICMSSN300;
+    int ICMSSN400;
+    int ICMSSN500;
+    int ICMSSN900;
+
+    public RecolhimentoIcms() {
+        // <editor-fold defaultstate="collapsed" desc="Compiled Code">
+        /* 0: aload_0
+         * 1: invokespecial java/lang/Object."<init>":()V
+         * 4: return
+         *  */
+        // </editor-fold>
+    }
+}
