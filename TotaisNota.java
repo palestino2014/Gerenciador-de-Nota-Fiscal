5e6591177b23f4360833ccb422b04fb02f971d6c
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-10 21:32:26 -0300

    Create TotaisNota.java

---
+++ TotaisNota.java
@@ -0,0 +1,28 @@
+package notafiscal;
+
+public class TotaisNota {
+
+    float infAdProd;
+    float vFrete;
+    float vSeg;
+    float Desc;
+    float vOutro;
+    float vICMS;
+    float vBC;
+    float vBCST;
+    float vST;
+    float vIPI;
+    float vPIS;
+    float vCOFINS;
+    float vNF;
+    float vTotTrib;
+
+    public TotaisNota() {
+        // <editor-fold defaultstate="collapsed" desc="Compiled Code">
+        /* 0: aload_0
+         * 1: invokespecial java/lang/Object."<init>":()V
+         * 4: return
+         *  */
+        // </editor-fold>
+    }
+}
