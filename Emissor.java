0d1e4313907be60b2b9cb356eee90329692e3b38
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-10 21:23:25 -0300

    Update Emissor.java

--- Emissor.java
+++ Emissor.java
@@ -9,8 +9,21 @@ package notafiscal;
  *
  * @author informatica
  */
+
 public class Emissor {
-    
-      String nome, endereco, telefone, CNPJ, RG, email, dataNascimento;
-    
-}
+
+    String cnpj;
+    String nomeRazaoSocial;
+    String nomeFantasia;
+    int incricaoEstadual;
+    int regimeTributario;
+    int cep;
+    String endereco;
+    String lougradoro;
+    int numero;
+    String complemento;
+    String bairro;
+    String estado;
+    String cidade;
+
+   }
