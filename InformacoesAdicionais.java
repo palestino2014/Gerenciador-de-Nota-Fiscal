3c764177075290aeb2669b650d150c0228327db9
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-10 21:33:44 -0300

    Create InformacoesAdicionais.java

---
+++ InformacoesAdicionais.java
@@ -0,0 +1,15 @@
+package notafiscal;
+
+public class InformacoesAdicionais {
+
+    String infAdProd;
+
+    public InformacoesAdicionais() {
+        // <editor-fold defaultstate="collapsed" desc="Compiled Code">
+        /* 0: aload_0
+         * 1: invokespecial java/lang/Object."<init>":()V
+         * 4: return
+         *  */
+        // </editor-fold>
+    }
+}
