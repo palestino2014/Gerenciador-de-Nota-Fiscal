4185d571fd721ccc80ad0f3b0aaa2d85db1da179
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-19 19:12:56 -0200

    Update InformacoesAdicionais.java

--- InformacoesAdicionais.java
+++ InformacoesAdicionais.java
@@ -1,46 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+public class InformacoesAdicionais {
 
-package notafiscal;
+    String infAdProd; // InformaÃ§Ãµes adicionais do produto
 
-/**
- *
- * @author gustavo
- */
-public class Endereco {
+    public String getinfAdProd() {
+        return infAdProd;
+    }
 
-        int cep;
-        String endereco;
-        String lougradouro;
-        int numero;
-        String complemento;
-        String estado;
-        String municipio ;
-        String bairro;
-        String cidade;
-
-        public int getCep() {
-        return cep;
-        }
-        public String getEndereco() {
-        return endereco;
-        }
-        public String getLougradouro() {
-        return lougradouro;
-        }
-        public int getNumero() {
-        return numero;
-        }
-        public String getComplemento() {
-        return complemento;
-        }
-        public String getEstado() {
-        return estado;
-        }
-        public String getMunicipio() {
-        return municipio;
-        }
 }
-
