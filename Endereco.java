aca595e031b82b83452550b7bf71f8d31bcbac5d
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-19 09:25:59 -0200

    Add files via upload

---
+++ Endereco.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package notafiscal;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Endereco {
+
+        int cep;
+        String endereco;
+        String lougradouro;
+        int numero;
+        String complemento;
+        String estado;
+        String municipio ;
+        String bairro;
+        String cidade;
+
+        public int getCep() {
+        return cep;
+        }
+        public String getEndereco() {
+        return endereco;
+        }
+        public String getLougradouro() {
+        return lougradouro;
+        }
+        public int getNumero() {
+        return numero;
+        }
+        public String getComplemento() {
+        return complemento;
+        }
+        public String getEstado() {
+        return estado;
+        }
+        public String getMunicipio() {
+        return municipio;
+        }
+}
