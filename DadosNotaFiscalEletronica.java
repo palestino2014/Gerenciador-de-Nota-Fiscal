586034c176c1f171d44028399c932a6db0b7deef
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2017-10-27 15:18:59 -0200

    Update DadosNotaFiscalEletronica.java

--- DadosNotaFiscalEletronica.java
+++ DadosNotaFiscalEletronica.java
@@ -11,55 +11,45 @@ package notafiscal;
  */
 public class DadosNotaFiscalEletronica {
 
-    int serie; // SequÃªncia numÃ©rica que indica a qual sÃ©rie a nota fiscal em questÃ£o se refere, de 001 a 999, sendo que o intervalo de 890 a 899 Ã© exclusivo para emissÃ£o de NF-e avulsa e, de 900 a 999, para emissÃ£o em contingÃªncia.
-    int nNF; //  Indica a numeraÃ§Ã£o da nota conforme a sÃ©rie, atendendo a uma sequÃªncia entre 1 a 999.999.999, sendo Ãºnico e exclusivo para um determinado documento fiscal.
-    String dhEmi; // Registra o dia e a hora exata em que a nota fiscal eletrÃ´nica foi emitida.
-    String dhSaiEnt; // Registra o dia e a hora exata em que ocorreu a movimentaÃ§Ã£o da mercadoria, deixando ou retornando Ã  empresa.
-    String tpNF; //  Indica basicamente se a operaÃ§Ã£o se refere Ã  entrada ou saÃ­da de mercadorias.
-    String indPag; //  Registra como a operaÃ§Ã£o em questÃ£o serÃ¡ paga pelo destinatÃ¡rio, considerando as opÃ§Ãµes â€œÃ  vistaâ€�, â€œa prazoâ€� e â€œoutrasâ€�.
-    String indPres; //  Esse campo se destina a informar se a operaÃ§Ã£o foi realizada de forma presencial ou nÃ£o, incluindo internet, telefone e outros meios.
-    String natOp; //  Indica se a operaÃ§Ã£o Ã  qual a nota se refere estÃ¡ destinada Ã  venda de mercadoria, remessa, devoluÃ§Ã£o ou consignaÃ§Ã£o, por exemplo.
-    String indFinal; //  Essa opÃ§Ã£o deve ser assinalada quando a operaÃ§Ã£o tem como destinatÃ¡rio um consumidor final e nÃ£o uma empresa que irÃ¡ revender a mercadoria.
-    String idDest; // Informa se a operaÃ§Ã£o Ã© interna (dentro do estado), interestadual ou com destinatÃ¡rio estabelecido no exterior.
+    int serie; // Sequência numérica que indica a qual série a nota fiscal em questão se refere, de 001 a 999, sendo que o intervalo de 890 a 899 é exclusivo para emissão de NF-e avulsa e, de 900 a 999, para emissão em contingência.
+    int nNF; //  Indica a numeração da nota conforme a série, atendendo a uma sequência entre 1 a 999.999.999, sendo único e exclusivo para um determinado documento fiscal.
+    String dhEmi; // Registra o dia e a hora exata em que a nota fiscal eletrônica foi emitida.
+    String dhSaiEnt; // Registra o dia e a hora exata em que ocorreu a movimentação da mercadoria, deixando ou retornando à empresa.
+    String tpNF; //  Indica basicamente se a operação se refere à entrada ou saída de mercadorias.
+    String indPag; //  Registra como a operação em questão será paga pelo destinatário, considerando as opções “à vista”, “a prazo” e “outras”.
+    String indPres; //  Esse campo se destina a informar se a operação foi realizada de forma presencial ou não, incluindo internet, telefone e outros meios.
+    String natOp; //  Indica se a operação à qual a nota se refere está destinada à venda de mercadoria, remessa, devolução ou consignação, por exemplo.
+    String indFinal; //  Essa opção deve ser assinalada quando a operação tem como destinatário um consumidor final e não uma empresa que irá revender a mercadoria.
+    String idDest; // Informa se a operação é interna (dentro do estado), interestadual ou com destinatário estabelecido no exterior.
 
            public int getserie() {
            return serie;
     }
-
            public int getnNF() {
            return nNF;
     }
-
             public String getdhEmi() {
             return dhEmi;
     }
-
             public String getdhSaiEnt() {
             return dhSaiEnt;
     }
-
             public String gettpNF() {
             return tpNF;
     }
-
             public String getindPag() {
             return indPag;
     }
-
             public String getindPres() {
             return indPres;
     }
-
             public String getnatOp() {
             return natOp;
     }
-
             public String getindFinal() {
             return indFinal;
     }
-
             public String getidDest() {
             return idDest;
     }
-
 }
